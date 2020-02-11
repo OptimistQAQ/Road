e452cbdeb838cab45254508cfb9a5d0a0c0ae405
OptimistQAQ <656673477@qq.com>
2020-02-11 16:13:17 +0800

    :bug: register activity

---
+++ app/src/main/java/com/example/a65667/road/activity/RegisterActivity.java
@@ -0,0 +1,37 @@
+package com.example.a65667.road.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.a65667.road.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ImageButton bt_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        init();
+
+        bt_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+            Intent signInActivity = new Intent(RegisterActivity.this, SignInActivity.class);
+            startActivity(signInActivity);
+        }
+        });
+    }
+
+    private void init(){
+        bt_register = (ImageButton)findViewById(R.id.bt_register);
+    }
+
+}
